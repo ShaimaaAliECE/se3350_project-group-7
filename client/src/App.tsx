553b85d3e1f842f280523843072e1f2686a4280c
Tony Xu <33339063+xutony05@users.noreply.github.com>
2022-01-29 17:45:13 -0500

    state for algo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { ChakraProvider, Select, Container } from '@chakra-ui/react'
 
 function App() {
+  const [algo, setAlgo] = useState("")
   return (
     <ChakraProvider>
       <Container style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
         <h1 style={{ textAlign: 'center' }}>Algorithmn Educational Game</h1>
         <br></br>
-        <Select placeholder='Select Algo'>
-          <option value='Merge Sort'>Merge Sort</option>
+        <Select onChange={(e) => setAlgo(e.target.value)} placeholder='Select Algo'>
+          <option value='MergeSort'>Merge Sort</option>
         </Select>
       </Container>
     </ChakraProvider >
