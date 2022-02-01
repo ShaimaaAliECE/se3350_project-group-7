210da1e7ba5fae061c937a9c1a12ad366b32255e
Amanda Adam <amanda.canada1@gmail.com>
2022-02-01 15:13:34 -0500

    level tabs
    level tabs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
 import { ChakraProvider, Select, Container } from "@chakra-ui/react";
+import Demo from "./components/Demo";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 
 function App() {
   const [algo, setAlgo] = useState("");
+  // return (
+    
+  //   <ChakraProvider>
+  //     <Demo/>
+  //   </ChakraProvider>
+  // );
   return (
     <ChakraProvider>
+<Tabs>
+  <TabList>
+    <Tab>Level 1</Tab>
+    <Tab>Level 2</Tab>
+    <Tab>Level 3</Tab>
+    <Tab>Level 4</Tab>
+    <Tab>Level 5</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel>
       <Container
         style={{
           display: "flex",
@@ -14,7 +32,7 @@ function App() {
           height: "100vh",
         }}
       >
-        <h1 style={{ textAlign: "center" }}>Algorithmn Educational Game</h1>
+        <h1 style={{ textAlign: "center" }}>Algorithmn Educational Game-</h1>
         <br></br>
         <Select
           onChange={(e) => setAlgo(e.target.value)}
@@ -23,6 +41,21 @@ function App() {
           <option value="MergeSort">Merge Sort</option>
         </Select>
       </Container>
+    </TabPanel>
+    <TabPanel>
+      <p>Level 2</p>
+    </TabPanel>
+    <TabPanel>
+      <p>Level 3</p>
+    </TabPanel>
+    <TabPanel>
+      <p>Level 4</p>
+    </TabPanel>
+    <TabPanel>
+      <p>Level 5</p>
+    </TabPanel>
+  </TabPanels>
+</Tabs> 
     </ChakraProvider>
   );
 }
