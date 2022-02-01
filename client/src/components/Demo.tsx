8cc01d4708d5caf8259e905bf6fdf265019d0915
Gareth Lau <Gareth.kh.lau@gmail.com>
2022-02-01 16:58:01 -0500

    Add spacing between array groups

--- client/src/components/Demo.tsx
+++ client/src/components/Demo.tsx
@@ -13,20 +13,28 @@ const Demo: React.FC<DemoProps> = ({}) => {
         <Button onClick={game.next}>Next</Button>
       </Box>
       {game.steps.map((step, index) => (
-        <Flex 
-          align = "baseline"
-          p={1} 
-          m={2} 
-          opacity = {index > game.stepIndex ? 0 : 1}
-          bg={index === game.stepIndex ? "blue.200" : "white"}>
-          {step.value.map((s) => 
-          s.map((ss) => <Box h={ss+"px"} w = "7px" bg="blue"/>)
-          // (
-          //   <Box borderColor="black" borderStyle="solid" borderWidth="1px">
-          //     {s.toString()}
-          //   </Box>
-          // ) 
-          )}
+        <Flex
+          align="baseline"
+          p={1}
+          m={2}
+          opacity={index > game.stepIndex ? 0 : 1}
+          bg={index === game.stepIndex ? "blue.200" : "white"}
+        >
+          {step.value.map((arr, index) => (
+            <>
+              {index !== 0 && <Box w="5px" />}
+              {arr.map((num) => (
+                <Box
+                  h={num * 6 + "px"}
+                  w="20px"
+                  bg="blue.500"
+                  borderStyle="solid"
+                  borderColor="black"
+                  borderWidth="2px"
+                />
+              ))}
+            </>
+          ))}
         </Flex>
       ))}
     </div>
