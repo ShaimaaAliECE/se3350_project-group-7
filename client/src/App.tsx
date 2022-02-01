630d3b295742aea96158f12d9b8194e3c4251aa1
Gareth Lau <Gareth.kh.lau@gmail.com>
2022-01-31 22:24:41 -0500

    Create custom hook to encapsulate game state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Box, Flex, Button } from "@chakra-ui/react";
+import useGame from "./hooks/useGame";
 
 function App() {
+  const game = useGame();
   return (
     <ChakraProvider>
-      <div>Hello SE3350!!!</div>
+      <Box>
+        <Button onClick={game.prev}>Prev</Button>
+        <Button onClick={game.next}>Next</Button>
+      </Box>
+      {game.steps.map((step, index) => (
+        <Flex p={1} m={2} bg={index === game.stepIndex ? "blue.200" : "white"}>
+          {step.map((s) => (
+            <Box borderColor="black" borderStyle="solid" borderWidth="1px">
+              {s.toString()}
+            </Box>
+          ))}
+        </Flex>
+      ))}
     </ChakraProvider>
   );
 }
