32c8100f73e3535e8b176c4d2ef9155ad0d20a33
jsong349 <jsong349@uwo.ca>
2022-02-01 22:21:06 -0500

    add spaces and numbers

--- client/src/components/Demo.tsx
+++ client/src/components/Demo.tsx
@@ -9,26 +9,40 @@ const Demo: React.FC<DemoProps> = ({}) => {
   const game = useGame();
   return (
     <div>
-      <Heading position= "fixed">Level 1: Demo of Merge Sort</Heading>
-      <Box position= "fixed" right= "0" >
-        <Button onClick={game.prev}>Prev</Button>
-        <Button onClick={game.next}>Next</Button>
-      </Box>
+      <Flex bg="white.100">
+        <Heading position= "fixed">Level 1: Demo of Merge Sort</Heading>
+        <Box position= "fixed" right= "0" >
+          <Button onClick={game.prev}>Prev</Button>
+          <Button onClick={game.next}>Next</Button>
+        </Box>
+      </Flex>
+      
+      <br></br>
+      <br></br>
       {game.steps.map((step, index) => (
         <Flex 
           align = "baseline"
+          text-align = "baseline"
           p={1} 
           m={2} 
           opacity = {index > game.stepIndex ? 0 : 1}
           bg={index === game.stepIndex ? "gray.200" : "white"}>
-          {step.value.map((s) => 
-          s.map((ss) => <Box h={ss+"1px"} w = "50px" bg="blue.200"/>)
-          // (
-          //   <Box borderColor="black" borderStyle="solid" borderWidth="1px">
-          //     {s.toString()}
-          //   </Box>
-          // ) 
-          )}
+          
+          {step.value.map((arr, index) => (
+            <>
+              {index !== 0 && <Box w="10px" />}
+              {arr.map((num) => (
+                <Box
+                  h={num * 6 + "px"}
+                  w="50px"
+                  bg="blue.300"
+                  text-align = "center"
+                >
+                {num.toString()}
+                </Box>
+              ))}
+            </>
+          ))}
         </Flex>
       ))}
     </div>
