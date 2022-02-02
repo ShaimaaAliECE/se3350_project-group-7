196d30dbf1f10081871e21c84d6aa5343f1a3c97
Ben Wolfman <benwolfman@gmail.com>
2022-02-01 23:02:02 -0500

    SG7-42 added verification for text input
    when a user types a guess in a text box for level 2, the guess is compared with the expected answer

--- client/src/components/Level2.tsx
+++ client/src/components/Level2.tsx
@@ -5,12 +5,21 @@ import useGame from "../hooks/useGame";
 export default function Level2() {
     const game = useGame();
     const [counter, setCounter] = useState(1);
+    function isValid(e:any, answer:any) {
+        console.log(e)
+        console.log(answer)
+        if (e === answer) {
+            console.log("correct")
+        } else {
+            console.log("wrong")
+        }
+    }
     return (
         <Container centerContent>
             <h1>{game.steps[0].value.toString()}</h1>
             <Center>
                 {game.steps[counter].value.map(arr => (
-                    <Input placeholder='Insert numbers' />
+                    <Input name={arr.toString()} onChange={(e) => isValid(e.target.value, arr.toString())} placeholder='Insert numbers' />
                 ))}
             </Center>
             <Button onClick={() => setCounter(counter + 1)}>Next Step</Button>
