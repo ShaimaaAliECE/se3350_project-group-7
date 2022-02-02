b41844ede0a9993a694ffca90f7e3a2adc33d142
Ben Wolfman <benwolfman@gmail.com>
2022-02-02 00:15:33 -0500

    SG7-44 added text UI
    Added text boxes to level 2 to allow user to enter guesses for the next steps

--- client/src/components/Level2.tsx
+++ client/src/components/Level2.tsx
@@ -1,14 +1,19 @@
-import React from "react";
-import { Box, Flex, Button, Container } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, Flex, Button, Container, Input, Center } from "@chakra-ui/react";
 import useGame from "../hooks/useGame";
 
 export default function Level2() {
     const game = useGame();
-    let i = 1
+    const [counter, setCounter] = useState(1);
     return (
         <Container centerContent>
             <h1>{game.steps[0].value.toString()}</h1>
-
+            <Center>
+                {game.steps[counter].value.map(arr => (
+                    <Input placeholder='Insert numbers' />
+                ))}
+            </Center>
+            <Button onClick={() => setCounter(counter + 1)}>Next Step</Button>
         </Container>
     )
 }
