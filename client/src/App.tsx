fb2f630199265a0111df79ec945e6efe57838fda
Amanda Adam <amanda.canada1@gmail.com>
2022-02-06 13:28:39 -0500

    created navigation component
    completed SG7-58 and 59: create a global navigation component which includes the tabs list and connected the nav tabs to the correct level pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/Home";
+import Navigation from "./components/navigation";
 
 function App() {
   return (
     <ChakraProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Navigation />} />
         </Routes>
       </Router>
     </ChakraProvider>
