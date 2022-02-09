efa99727aef901b0deb5f982c4940fa927c135c6
Ben Wolfman <benwolfman@gmail.com>
2022-02-09 13:40:21 -0500

    Placed Router within GameProvider tag

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { GameProvider } from "./context/GameContext";
 import Navigation from "./components/navigation";
 
 function App() {
   return (
     <ChakraProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigation />} />
-        </Routes>
-      </Router>
+        <GameProvider>
+            <Router>
+                <Routes>
+                <Route path="/" element={<Navigation />} />
+                </Routes>
+            </Router>
+        </GameProvider>
     </ChakraProvider>
   );
 }
