95d0b8701834589ec8aea6b649ae9861ca04492a
Aradhya Bansal <aradhyabansal2000@gmail.com>
2022-02-10 13:04:16 -0500

    Modify Header component to jump to levels on tab click and to render correct level components in panels

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,43 +1,50 @@
 import React from "react";
-import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@chakra-ui/react";
+import { Tabs, TabList, Tab, TabPanels, TabPanel, Box } from "@chakra-ui/react";
 import Home from "../pages/Home";
-import Level2 from "./Level2";
-import Demo from "./Demo";
+import Level1 from "@/components/Level1";
+import Level2 from "@/components/Level2";
+import Level3 from "@/components/Level3";
+import Level4 from "@/components/Level4";
+import Level5 from "@/components/Level5";
+import { useGame } from "@/context/GameContext";
 
 export type Props = {};
 
 const Header = () => {
+  // TODO: Remove this hook once next level is properly implemented.
+  // Tabs should not be clickable, this is only for demo purpose.
+  const game = useGame();
   return (
     <Tabs>
-      <TabList>
-        <Tab>Home</Tab>
-        <Tab>Level 1</Tab>
-        <Tab>Level 2</Tab>
-        <Tab>Level 3</Tab>
-        <Tab>Level 4</Tab>
-        <Tab>Level 5</Tab>
-      </TabList>
+      <Box bg="gray.200" w="100%" position="sticky" zIndex="1" p={4}>
+        <TabList>
+          <Tab>Home</Tab>
+          <Tab onClick={() => game.jumpToLevel(0)}>Level 1</Tab>
+          <Tab onClick={() => game.jumpToLevel(1)}>Level 2</Tab>
+          <Tab onClick={() => game.jumpToLevel(2)}>Level 3</Tab>
+          <Tab onClick={() => game.jumpToLevel(3)}>Level 4</Tab>
+          <Tab onClick={() => game.jumpToLevel(4)}>Level 5</Tab>
+        </TabList>
+      </Box>
       <TabPanels>
         <TabPanel>
           <Home />
         </TabPanel>
         <TabPanel>
-          <Demo />
+          <Level1 />
         </TabPanel>
         <TabPanel>
           <Level2 />
         </TabPanel>
-        {/* TabPanels for additional levels are below */}
-        {/* <TabPanel> */}
-        {/* </TabPanel> */}
-        {/* <TabPanel> */}
-        {/* </TabPanel> */}
-        {/* <TabPanel> */}
-        {/* </TabPanel> */}
-        {/* <TabPanel> */}
-        {/* </TabPanel> */}
-        {/* <TabPanel> */}
-        {/* </TabPanel> */}
+        <TabPanel>
+          <Level3 />
+        </TabPanel>
+        <TabPanel>
+          <Level4 />
+        </TabPanel>
+        <TabPanel>
+          <Level5 />
+        </TabPanel>
       </TabPanels>
     </Tabs>
   );
