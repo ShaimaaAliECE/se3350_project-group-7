0fdd3ee7478e6050294ff87c55cec49881590c70
Aradhya Bansal <aradhyabansal2000@gmail.com>
2022-02-10 13:00:19 -0500

    Add StepValidation as a component to generate input boxes based on context

---
+++ client/src/components/StepValidation.tsx
@@ -0,0 +1,32 @@
+import { useGame } from "@/context/GameContext";
+import { Flex, Input } from "@chakra-ui/react";
+
+const StepValidation = () => {
+  const game = useGame();
+  return (
+    <Flex mt={6}>
+      {game.stepIndex < game.steps.length - 1 &&
+        game.steps[game.stepIndex + 1].value.map((arr, index) => (
+          <Input
+            ml={2}
+            mr={2}
+            value={game.values[index] || ""}
+            onChange={(e) => game.handleInput(index, e.target.value)}
+            isDisabled={game.correct[index]}
+            isReadOnly={game.readOnly[index] || false}
+            focusBorderColor={game.correct[index] ? "lime" : "blue.200"}
+            borderColor={
+              game.correct[index]
+                ? "lime"
+                : !game.correct[index] && game.correct[index] !== undefined
+                ? "red.500"
+                : "grey"
+            }
+            placeholder={"Insert numbers"}
+          />
+        ))}
+    </Flex>
+  );
+};
+
+export default StepValidation;
