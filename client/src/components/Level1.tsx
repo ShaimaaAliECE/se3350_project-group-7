cbe143b32214289fc76b0fb9d1591f8793918820
Aradhya Bansal <aradhyabansal2000@gmail.com>
2022-02-10 13:02:34 -0500

    Add components to render all levels using the LevelLayout

---
+++ client/src/components/Level1.tsx
@@ -0,0 +1,14 @@
+import LevelLayout from "./LevelLayout";
+import React from "react";
+
+const Level1 = () => {
+  return (
+    <LevelLayout
+      headingText="Level 1: Sorting Demonstration"
+      showInstructions={true}
+      showInput={false}
+    />
+  );
+};
+
+export default Level1;
---
+++ client/src/components/Level5.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LevelLayout from "./LevelLayout";
+
+const Level5 = () => {
+  return (
+    <LevelLayout
+      headingText="Level 5: Extra-Large User-Driven Sort"
+      showInstructions={false}
+      showInput={true}
+    />
+  );
+};
+
+export default Level5;
