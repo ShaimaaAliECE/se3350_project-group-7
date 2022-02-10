e936e7e5c20b93cda5a22c12f9bb7c3c16d6bbad
Aradhya Bansal <aradhyabansal2000@gmail.com>
2022-02-10 10:29:44 -0500

    Rename navigation to Header

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@chakra-ui/react";
+import Home from "../pages/Home";
+import Level2 from "./Level2";
+import Demo from "./Demo";
+
+export type Props = {};
+
+const Header = () => {
+  return (
+    <Tabs>
+      <TabList>
+        <Tab>Home</Tab>
+        <Tab>Level 1</Tab>
+        <Tab>Level 2</Tab>
+        <Tab>Level 3</Tab>
+        <Tab>Level 4</Tab>
+        <Tab>Level 5</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <Home />
+        </TabPanel>
+        <TabPanel>
+          <Demo />
+        </TabPanel>
+        <TabPanel>
+          <Level2 />
+        </TabPanel>
+        {/* TabPanels for additional levels are below */}
+        {/* <TabPanel> */}
+        {/* </TabPanel> */}
+        {/* <TabPanel> */}
+        {/* </TabPanel> */}
+        {/* <TabPanel> */}
+        {/* </TabPanel> */}
+        {/* <TabPanel> */}
+        {/* </TabPanel> */}
+        {/* <TabPanel> */}
+        {/* </TabPanel> */}
+      </TabPanels>
+    </Tabs>
+  );
+};
+
+export default Header;
