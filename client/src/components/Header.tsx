fcca70e90d4bbb6f0244b521d11d760ed9b8be55
jsong349 <jsong349@uwo.ca>
2022-02-10 17:46:02 -0500

    Format code

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { Tabs, TabList, Tab, TabPanels, TabPanel, Spacer, Flex, Button } from "@chakra-ui/react";
+import {
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Spacer,
+  Flex,
+  Button,
+} from "@chakra-ui/react";
 import Level1 from "@/components/Level1";
 import Level2 from "@/components/Level2";
 import Level3 from "@/components/Level3";
 import Level4 from "@/components/Level4";
 import Level5 from "@/components/Level5";
 import { useGame } from "@/context/GameContext";
-import {useNavigate} from "react-router"
+import { useNavigate } from "react-router";
 
 export type Props = {};
-
-
 
 const Header = () => {
   // TODO: Remove this hook once next level is properly implemented.
@@ -20,38 +27,41 @@ const Header = () => {
   const routeChange = () => {
     let path = "../";
     navigate(path);
-  }
+  };
+
   return (
-      <Tabs>
-        <Flex bg="gray.200" w="100%" position="sticky" zIndex="1" p={4}>
-          <TabList>
-            <Tab onClick={() => game.jumpToLevel(0)}>Level 1</Tab>
-            <Tab onClick={() => game.jumpToLevel(1)}>Level 2</Tab>
-            <Tab onClick={() => game.jumpToLevel(2)}>Level 3</Tab>
-            <Tab onClick={() => game.jumpToLevel(3)}>Level 4</Tab>
-            <Tab onClick={() => game.jumpToLevel(4)}>Level 5</Tab>
-          </TabList>
-          <Spacer /> 
-          <Button colorScheme='red'  onClick={routeChange}>Quit</Button>
-        </Flex>
-        <TabPanels>
-          <TabPanel>
-            <Level1 />
-          </TabPanel>
-          <TabPanel>
-            <Level2 />
-          </TabPanel>
-          <TabPanel>
-            <Level3 />
-          </TabPanel>
-          <TabPanel>
-            <Level4 />
-          </TabPanel>
-          <TabPanel>
-            <Level5 />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+    <Tabs>
+      <Flex bg="gray.200" w="100%" position="sticky" zIndex="1" p={4}>
+        <TabList>
+          <Tab onClick={() => game.jumpToLevel(0)}>Level 1</Tab>
+          <Tab onClick={() => game.jumpToLevel(1)}>Level 2</Tab>
+          <Tab onClick={() => game.jumpToLevel(2)}>Level 3</Tab>
+          <Tab onClick={() => game.jumpToLevel(3)}>Level 4</Tab>
+          <Tab onClick={() => game.jumpToLevel(4)}>Level 5</Tab>
+        </TabList>
+        <Spacer />
+        <Button colorScheme="red" onClick={routeChange}>
+          Quit
+        </Button>
+      </Flex>
+      <TabPanels>
+        <TabPanel>
+          <Level1 />
+        </TabPanel>
+        <TabPanel>
+          <Level2 />
+        </TabPanel>
+        <TabPanel>
+          <Level3 />
+        </TabPanel>
+        <TabPanel>
+          <Level4 />
+        </TabPanel>
+        <TabPanel>
+          <Level5 />
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   );
 };
 
