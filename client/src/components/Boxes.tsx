14cf895af28796a4aa6a66dd42b17e91f861b4a2
Aradhya Bansal <aradhyabansal2000@gmail.com>
2022-02-10 12:59:11 -0500

    Modify size of Box in Boxes to be smaller on Level 5, when there are 50 numbers on screen

--- client/src/components/Boxes.tsx
+++ client/src/components/Boxes.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Flex } from "@chakra-ui/react";
+import { useGame } from "@/context/GameContext";
 
 export type BoxesProps = { values: number[] };
 
 const Boxes: React.FC<BoxesProps> = ({ values }) => {
+  const game = useGame();
   return (
     <Flex>
       {values.map((num, index) => (
@@ -12,8 +14,8 @@ const Boxes: React.FC<BoxesProps> = ({ values }) => {
           borderWidth="2px"
           borderColor="blue.500"
           borderStyle="solid"
-          h="50px"
-          w="50px"
+          h={game.level === 4 ? "28px" : "50px"}
+          w={game.level === 4 ? "28px" : "50px"}
           bg="blue.300"
           justify="center"
           align="center"
