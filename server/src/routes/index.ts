2c94147421a39fce0f9037ed32d4c6fdfe750e2a
Gareth Lau <Gareth.kh.lau@gmail.com>
2022-02-13 01:48:11 -0500

    Implement `/api/ping` route for testing and health checking

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
+import { getId } from "../lib/id";
 
 // Configure all routes
 const router = express.Router();
+
+router.get("/ping", (req, res) => {
+  const id = getId(req);
+  return res.status(200).send({ message: "OK", id });
+});
 
 export default router;
