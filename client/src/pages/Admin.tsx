f4dd136da46a36cf9aa1d8a4149f7e060d3f2607
Amanda Adam <amanda.canada1@gmail.com>
2022-03-17 10:12:49 -0400

    created admin layout
    created admin layout. Need to figure out how to reference the level times.

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -3,33 +3,84 @@ import {
   Text,
   Stack,
   Heading,
-  Button
-} from '@chakra-ui/react';
+  Button,
+  Box,
+  chakra,
+  SimpleGrid,
+  Stat,
+  StatLabel,
+  StatNumber,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
 import { useNavigate } from "react-router-dom";
 
-const Admin = () =>{
-    const navigate = useNavigate();
-        const routeChange = () => {
-          let path = "/";
-          navigate(path);
-        };
-    return (
-        <Stack>
-            <Heading
-            fontWeight={600}
-            fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
-            lineHeight={'110%'}>
-            <Text as={'span'} color={'orange.400'}>
-            Player Analytics
-            </Text>
-            <Button colorScheme="orange" onClick={routeChange}>
-            Home
-            </Button>
-            </Heading>
-        </Stack>
-        
-    )
-};
+interface StatsCardProps {
+  title: string;
+  stat: string;
+}
 
+function StatsCard(props: StatsCardProps) {
+  const { title, stat } = props;
+  return (
+    <Stat
+      px={{ base: 4, md: 8 }}
+      py={"5"}
+      border={"1px solid"}
+      borderColor={useColorModeValue("orange.400", "orange.400")}
+      rounded={"lg"}
+    >
+      <StatLabel fontWeight={"medium"} isTruncated>
+        {title}
+      </StatLabel>
+      <StatNumber fontSize={"2xl"} fontWeight={"medium"}>
+        {stat}
+      </StatNumber>
+    </Stat>
+  );
+}
+
+const Admin = () => {
+  const navigate = useNavigate();
+  const routeChange = () => {
+    let path = "/";
+    navigate(path);
+  };
+  return (
+    <Stack>
+      <Heading
+        fontWeight={600}
+        fontSize={{ base: "3xl", sm: "4xl", md: "6xl" }}
+        lineHeight={"110%"}
+        padding-left={"50px"}
+      >
+        <Button colorScheme="orange" onClick={routeChange}>
+          Home
+        </Button>
+      </Heading>
+
+      <Box maxW="7xl" mx={"auto"} pt={5} px={{ base: 2, sm: 12, md: 17 }}>
+        <chakra.h1
+          textAlign={"center"}
+          fontSize={"4xl"}
+          py={10}
+          fontWeight={"bold"}
+        >
+          <Text as={"span"} color={"orange.400"}>
+            Player Analytics
+          </Text>
+        </chakra.h1>
+        <SimpleGrid columns={{ base: 1, md: 3 }} spacing={{ base: 5, lg: 8 }}>
+          <StatsCard title={"Time Spent on Level 1"} stat={"5 minutes"} />
+          <StatsCard title={"Time Spent on Level 2"} stat={"5 minutes"} />
+          <StatsCard title={"Time Spent on Level 3"} stat={"5 minutes"} />
+          <StatsCard title={"Time Spent on Level 4"} stat={"5 minutes"} />
+          <StatsCard title={"Time Spent on Level 5"} stat={"5 minutes"} />
+          <StatsCard title={"Time Spent Overall"} stat={"25 minutes"} />
+        </SimpleGrid>
+      </Box>
+    </Stack>
+  );
+};
 
 export default Admin;
