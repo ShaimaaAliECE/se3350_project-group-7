52ab308801b569cc824d2bd66f6c96e3fe6b9b8e
jsong349 <jsong349@uwo.ca>
2022-03-24 17:08:25 -0400

    Format code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           <Routes>
             <Route path="/game" element={<SortingGame />} />
             <Route path="/" element={<Home />} />
-            <Route path= "/login" element={<LoginPage />} />
+            <Route path="/login" element={<LoginPage />} />
           </Routes>
         </Router>
       </GameProvider>
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,60 +1,65 @@
 import React, { useState } from "react";
-import {
-    Text,
-    Heading,
-    Button,
-    Input,
-  } from "@chakra-ui/react";
+import { Text, Heading, Button, Input } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
-import { useToast } from '@chakra-ui/react'
+import { useToast } from "@chakra-ui/react";
 import { reduceEachTrailingCommentRange } from "typescript";
 
 const LoginPage = () => {
-    
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const toast = useToast()
-  
-    const navigate = useNavigate();
-    const routeChange = () => {
-        let path = "/yay";
-        navigate(path);
-    }
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const toast = useToast();
 
-    function handleUsername(e: React.SyntheticEvent<HTMLInputElement>) {
-        const value = e.currentTarget.value;
-        setUsername(value);
-    }
+  const navigate = useNavigate();
+  const routeChange = () => {
+    let path = "/yay";
+    navigate(path);
+  };
 
-    function handlePassword(e: React.SyntheticEvent<HTMLInputElement>) {
-        const value = e.currentTarget.value;
-        setPassword(value);
-    }
+  function handleUsername(e: React.SyntheticEvent<HTMLInputElement>) {
+    const value = e.currentTarget.value;
+    setUsername(value);
+  }
 
-    function handleSubmit() {
-        if(username=="admin" && password=="root"){
-            routeChange();
-        }else{
-            toast({
-                title: 'ERROR',
-                description: "You've entered an incorrect username or password. Please try again.",
-                status: 'error',
-                duration: 9000,
-                isClosable: true,
-              })
-        }
-    }
+  function handlePassword(e: React.SyntheticEvent<HTMLInputElement>) {
+    const value = e.currentTarget.value;
+    setPassword(value);
+  }
 
-    return(
-        <div>
-            <Heading>Time to Login</Heading>
-            <Text>Username: </Text>
-            <Input onChange={handleUsername} value={username} placeholder='medium size' size='md' />
-            <Text>Password: </Text>
-            <Input onChange={handlePassword} value={password} placeholder='medium size' size='md' />
-            <Button onClick={handleSubmit}>Login</Button>
-        </div>
-    );
-}
+  function handleSubmit() {
+    if (username == "admin" && password == "root") {
+      routeChange();
+    } else {
+      toast({
+        title: "ERROR",
+        description:
+          "You've entered an incorrect username or password. Please try again.",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  }
+
+  return (
+    <div>
+      <Heading>Time to Login</Heading>
+      <Text>Username: </Text>
+      <Input
+        onChange={handleUsername}
+        value={username}
+        placeholder="medium size"
+        size="md"
+      />
+      <Text>Password: </Text>
+      <Input
+        onChange={handlePassword}
+        value={password}
+        placeholder="medium size"
+        size="md"
+      />
+      <Button onClick={handleSubmit}>Login</Button>
+    </div>
+  );
+};
 
 export default LoginPage;
