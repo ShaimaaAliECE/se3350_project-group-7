4d7c87e2bb0f20410fee06dd0f01b350bdd23b97
jsong349 <jsong349@uwo.ca>
2022-03-24 17:02:44 -0400

    Add logging checkign

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,21 +1,47 @@
-import React, { useState } from "react";
+import React, { HTMLInputTypeAttribute, useState } from "react";
 import {
     Text,
     Heading,
     Button,
     Input,
   } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
     
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+  
+    const navigate = useNavigate();
+    const routeChange = () => {
+        let path = "/yay";
+        navigate(path);
+    }
+
+    function handleUsername(e: React.SyntheticEvent<HTMLInputElement>) {
+        const value = e.currentTarget.value;
+        setUsername(value);
+    }
+
+    function handlePassword(e: React.SyntheticEvent<HTMLInputElement>) {
+        const value = e.currentTarget.value;
+        setPassword(value);
+    }
+
+    function handleSubmit() {
+        if(username=="admin" && password=="root"){
+            routeChange();
+        }
+    }
+
     return(
         <div>
             <Heading>Time to Login</Heading>
             <Text>Username: </Text>
-            <Input placeholder='medium size' size='md' />
+            <Input onChange={handleUsername} value={username} placeholder='medium size' size='md' />
             <Text>Password: </Text>
-            <Input placeholder='medium size' size='md' />
-            <Button>Login</Button>
+            <Input onChange={handlePassword} value={password} placeholder='medium size' size='md' />
+            <Button onClick={handleSubmit}>Login</Button>
         </div>
     );
 }
